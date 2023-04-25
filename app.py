159e22f06c160555eea90840cf17d0a268f74e61
q.c <chenqin@cardc.cn>
2023-04-25 20:41:21 +0800

    Update sliders for parameters, no button

--- app.py
+++ app.py
@@ -13,13 +13,11 @@ Parafoil Dynamics for Fun
     by Qin Chen, Xing-long Gao
 """
 
-d_omega_deg = st.slider("Maximum control input", 5.0, 20.0, 10.0, 1.0)
-omega_0_deg = st.slider("Initial flight angle", -180.0, 180.0, -15.0, 5.0)
+d_omega_deg = st.slider("Maximum control input (°/s)", 5.0, 20.0, 10.0, 1.0)
+omega_0_deg = st.slider("Initial flight angle (°)", -180.0, 180.0, -15.0, 5.0)
 n_collocation_points = st.slider("Number of collocation points", 5, 100, 25, 1)
 
-if st.button("Flight a parafoil!"):
-
-
+if st.button("Have a go!"):
     p = Parafoil(450, 450, 9.0, 7.0, 1200.0, np.deg2rad(omega_0_deg))
 
     # p.strategy = HomingStrategy(0.1)
