924bd60cd1b21921afd6dcf86c1ed35b2ea3ee24
q.c <chenqin@cardc.cn>
2023-05-01 08:27:19 +0800

    Fix upload

--- dm/__init__.py
+++ dm/__init__.py
@@ -1,14 +1,15 @@
 import paramiko
-
-ssh = paramiko.SSHClient()
+import scp
 
 
 def upload(username: str, password: str, local_path: str, remote_path: str, host: str = "windtunnel.cn",
            port: int = 22):
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(host, port, username, password)
-    s = paramiko.SFTPClient.from_transport(ssh.get_transport())
-    s.put(local_path, remote_path)
-    ssh.close()
-
-
+    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # ssh.connect(host, port, username, password)
+    # s = paramiko.SFTPClient.from_transport(ssh.get_transport())
+    # s.put(local_path, remote_path)
+    # ssh.close()
+    with paramiko.Transport((host, port)) as transport:
+        transport.connect(username=username, password=password)
+        with scp.SCPClient(transport) as scp_client:
+            scp_client.put(local_path, remote_path, recursive=True)
